e6674b638120cbd4b1c4e81d034c77f6a902cf7e
Su-Yong <simssy2205@gmail.com>
2022-07-31 02:50:12 -0700

    feature: babel

--- packages/babel/src/index.ts
+++ packages/babel/src/index.ts
@@ -1,91 +1,11 @@
-import {
-  NodePath,
-  PluginObj, types
-} from '@babel/core';
-import { getUniqueId } from './utils/uniqueId';
-import { extractExpression, isExpression } from './utils/isExpression';
-import { DehyphenProcessor, AliasProcessor, Value } from '@suyongs/react-native-css';
-import { viewAliasMatcher } from '@suyongs/react-native-css/src/processor/alias/view';
+import { PluginObj } from '@babel/core';
+import visitor from './visitor';
 
-import { parse, Element } from '@suyongs/react-native-css';
-import valueResolver, { ValueResolverContext } from './resolver/valueResolver';
-import { Program } from '@babel/types';
+type PluginType = () => PluginObj;
 
-declare type BabelTypes = typeof import('@babel/types');
-type PluginType = ({ types }: { types: BabelTypes }) => PluginObj;
-
-const plugin: PluginType = ({ types: t }) => {
-  const processors = [
-    new DehyphenProcessor(),
-    new AliasProcessor(viewAliasMatcher),
-  ];
-
-  let root: NodePath<Program> | null = null;
-
-  return {
-    visitor: {
-      Program(path) {
-        root = path;
-      },
-      TaggedTemplateExpression(path) {
-        if (path.node.tag.type === 'Identifier' && path.node.tag.name === 'css') {
-          let result = '';
-
-          const interpolate = new Map<string, types.Expression>();
-          path.node.quasi.quasis.forEach((quasi, index) => {
-            let str = quasi.value.raw;
-            
-            const expression = path.node.quasi.expressions[index];
-            if (expression) {
-              const id = '--' + getUniqueId();
-              const placeholder = `expression(${id})`;
-              str += placeholder;
-
-              if (t.isTSType(expression)) throw Error('interpolator must be "Expression" not "TSType"');
-              interpolate.set(id, expression);
-            }
-            
-            result += str;
-          });
-
-          let ast: Element[] = parse(result);
-          processors.forEach((processor) => {
-            let newTree: Element[] = [];
-
-            ast.forEach((element) => {
-              const elements = processor.process(element);
-
-              if (Array.isArray(elements)) newTree.push(...elements);
-              else newTree.push(elements);
-            });
-
-            ast = newTree;
-          });
-
-          console.log('ast', JSON.stringify(ast, null, 2));
-
-          const resolverContext: ValueResolverContext = {
-            type: t,
-            expressionMapper: interpolate,
-          };
-          const properties: types.ObjectProperty[] = [];
-          ast.forEach((node) => {
-            if (node.type === 'declaration') {
-              if (node.values.length > 1) throw Error(`React Native Style can have only one parameter (${node.values.length} parameters provided)`);
-
-              const key = node.property;
-              const [value, runner] = valueResolver(node.values[0], resolverContext);
-
-              if (runner && root) runner(root, path);
-              properties.push(t.objectProperty(t.identifier(key), value));
-            }
-          });
-
-          path.replaceWith(t.objectExpression(properties));
-        }
-      },
-    },
-  };
-};
+const plugin: PluginType = () => ({
+  name: '@suyongs/babel-plugin-react-native-css',
+  visitor,
+});
 
 export default plugin;
---
+++ packages/babel/src/visitor.ts
@@ -0,0 +1,80 @@
+import { NodePath, Visitor, PluginPass, types } from '@babel/core';
+import { DehyphenProcessor, AliasProcessor } from '@suyongs/react-native-css';
+import { viewAliasMatcher } from '@suyongs/react-native-css/src/processor/alias/view';
+import valueResolver, { ValueResolverContext } from './resolver/valueResolver';
+import { getUniqueId } from './utils/uniqueId';
+import { parse, Element } from '@suyongs/react-native-css';
+
+import { types as t } from '@babel/core';
+import { Program } from '@babel/types';
+
+const processors = [
+  new DehyphenProcessor(),
+  new AliasProcessor(viewAliasMatcher),
+];
+
+let root: NodePath<Program> | null = null;
+const visitor: Visitor<PluginPass> = {
+  Program(path) {
+    root = path;
+  },
+  TaggedTemplateExpression(path, state) {
+    if (path.node.tag.type === 'Identifier' && path.node.tag.name === 'css') {
+      let result = '';
+
+      const interpolate = new Map<string, types.Expression>();
+      path.node.quasi.quasis.forEach((quasi, index) => {
+        let str = quasi.value.raw;
+        
+        const expression = path.node.quasi.expressions[index];
+        if (expression) {
+          const id = '--' + getUniqueId();
+          const placeholder = `expression(${id})`;
+          str += placeholder;
+
+          if (t.isTSType(expression)) throw Error('interpolator must be "Expression" not "TSType"');
+          interpolate.set(id, expression);
+        }
+        
+        result += str;
+      });
+
+      let ast: Element[] = parse(result);
+      processors.forEach((processor) => {
+        let newTree: Element[] = [];
+
+        ast.forEach((element) => {
+          const elements = processor.process(element);
+
+          if (Array.isArray(elements)) newTree.push(...elements);
+          else newTree.push(elements);
+        });
+
+        ast = newTree;
+      });
+
+      console.log('ast', JSON.stringify(ast, null, 2));
+
+      const resolverContext: ValueResolverContext = {
+        type: t,
+        expressionMapper: interpolate,
+      };
+      const properties: types.ObjectProperty[] = [];
+      ast.forEach((node) => {
+        if (node.type === 'declaration') {
+          if (node.values.length > 1) throw Error(`React Native Style can have only one parameter (${node.values.length} parameters provided)`);
+
+          const key = node.property;
+          const [value, runner] = valueResolver(node.values[0], resolverContext);
+
+          if (runner && root) runner(root, path);
+          properties.push(t.objectProperty(t.identifier(key), value));
+        }
+      });
+
+      path.replaceWith(t.objectExpression(properties));
+    }
+  },
+};
+
+export default visitor;
